425ed19fbd9f09af537f6aaefc73105cbc94c67d
CodeHedgehog <codehedgehog@outlook.com>
2018-09-02 16:36:51 -0700

    End of Module 3 Getting Resources

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿namespace Library.API.Controllers
+{
+	using AutoMapper;
+	using Library.API.Entities;
+	using Library.API.Models;
+	using Library.API.Services;
+	using Microsoft.AspNetCore.Mvc;
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+
+	[Route("api/authors")]
+	public class AuthorsController : Controller
+	{
+		private ILibraryRepository _libraryRepository;
+
+		public AuthorsController(ILibraryRepository libraryRepository)
+		{
+			_libraryRepository = libraryRepository;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAuthors()
+		{
+			// throw new Exception("Random exception for testing purpose");
+			IEnumerable<Author> authorsFromRepo = await Task.FromResult(_libraryRepository.GetAuthors());
+			IEnumerable<AuthorDto> authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+			return Ok(authors);
+		}
+
+		[HttpGet(template: "{id}", Name = "GetAuthor")]
+		public async Task<IActionResult> GetAuthor(Guid id)
+		{
+			Author authorFromRepo = await Task.FromResult(_libraryRepository.GetAuthor(id));
+			if (authorFromRepo == null) { return NotFound(); }
+			AuthorDto author = Mapper.Map<AuthorDto>(authorFromRepo);
+			return Ok(author);
+		}
+
+	}
+}
---
+++ src/Library.API/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿namespace Library.API.Controllers
+{
+	using AutoMapper;
+	using Library.API.Entities;
+	using Library.API.Models;
+	using Library.API.Services;
+	using Microsoft.AspNetCore.Mvc;
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+
+	[Route("api/authors/{authorId}/books")]
+	public class BooksController : Controller
+	{
+		private ILibraryRepository _libraryRepository;
+
+		public BooksController(ILibraryRepository libraryRepository)
+		{
+			_libraryRepository = libraryRepository;
+		}
+
+		[HttpGet()]
+		public async Task<IActionResult> GetBooksForAuthor(Guid authorId)
+		{
+			if (!_libraryRepository.AuthorExists(authorId)) return NotFound();
+			IEnumerable<Book> booksForAuthorFromRepo = await Task.FromResult(_libraryRepository.GetBooksForAuthor(authorId));
+			IEnumerable<BookDto> booksForAuthor = Mapper.Map<IEnumerable<BookDto>>(booksForAuthorFromRepo);
+			return Ok(booksForAuthor);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetBookForAuthor(Guid authorId, Guid id)
+		{
+			if (!_libraryRepository.AuthorExists(authorId)) return NotFound();
+			Book bookForAuthorFromRepo = await Task.FromResult(_libraryRepository.GetBookForAuthor(authorId, id));
+			if (bookForAuthorFromRepo == null) return NotFound();
+			BookDto bookForAuthor = Mapper.Map<BookDto>(bookForAuthorFromRepo);
+			return Ok(bookForAuthor);
+		}
+	}
+}
